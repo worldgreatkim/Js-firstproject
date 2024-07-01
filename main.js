8446d329b424fc5a15e4f298197c72d492d5fc72
worldgreatkim <edwin.dkim@gmail.com>
2024-07-02 00:41:16 +0900

    second-commit

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
-let computerNum = 0
 
-let playButton = document.getElementById("play-button")
+let computerNum = 0;
 
-let resultArea = document.getElementById("result-area")
-console.log(playButton)
-playButton.addEventListener("click", play)
+let playButton = document.getElementById("play-button");
+let resultArea = document.getElementById("result-area");
+let userInput = document.getElementById("user-input");
 
-let userInput = document.getElementById("user-input")
+playButton.addEventListener("click", play);
+
+pickRandomNum();
+
 function pickRandomNum() {
-    computerNum = Math.floor(Math.random()*100)+1;
+    computerNum = Math.floor(Math.random() * 100) + 1;
     console.log("정답", computerNum);
 }
 
 function play() {
-   let userValue = userInput.value;
-   if(userValue < computerNum) {
-    resultArea.textContent = "UP"
-        console.log("up")
-   }else if(userValue > computerNum){
-        resultArea.textContent = "DOWN"
-   }else{
-    resultArea.textContent = "맞췄습니다."
     
-   }
+    let userValue = parseInt(userInput.value, 10);
+    
+    if (userValue < computerNum) {
+        resultArea.textContent = "UP";
+        console.log("up");
+    } else if (userValue > computerNum) {
+        resultArea.textContent = "DOWN";
+        console.log("down");
+    } else {
+        resultArea.textContent = "맞췄습니다.";
+        console.log("맞췄습니다.");
+    }
 }
