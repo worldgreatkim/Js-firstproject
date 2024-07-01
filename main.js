1adfa06491a2e7b898aa129efa188074f6dd4749
worldgreatkim <edwin.dkim@gmail.com>
2024-07-02 01:47:41 +0900

    third-commit

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let resultArea = document.getElementById("result-area");
 let userInput = document.getElementById("user-input");
+let resetButton = document.getElementById("reset-button");
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history=[]
 
 playButton.addEventListener("click", play);
-
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", function(){userInput.value=""})
 pickRandomNum();
 
 function pickRandomNum() {
@@ -16,8 +22,19 @@ function pickRandomNum() {
 
 function play() {
     
-    let userValue = parseInt(userInput.value, 10);
-    
+    let userValue = userInput.value;
+    if(userValue<1 || userValue>100){
+        resultArea.textContent="1과 100사이 숫자를 입력해주세요."
+        return;
+    }
+    if(history.includes(userValue))
+        {
+            resultArea.textContent ="이미 입력한 숫자입니다. 다른 숫자를 입력해주세요";
+            return;
+        }
+    chances--;
+    chanceArea.textContent=`남은기회:${chances}번`;
+    console.log("chance", chances)
     if (userValue < computerNum) {
         resultArea.textContent = "UP";
         console.log("up");
@@ -27,5 +44,21 @@ function play() {
     } else {
         resultArea.textContent = "맞췄습니다.";
         console.log("맞췄습니다.");
+        gameOver=true;  
     }
+    history.push(userValue);
+    console.log(history);
+    if(chances < 1) {
+        gameOver = true;
+
+    }
+    if(gameOver == true) {
+        playButton.disabled = true;
+    }
+}
+
+function reset(){
+    userInput.value = ""
+    pickRandomNum();
+    resultArea.textContent = "결과값이 여기 나옵니다.";
 }
